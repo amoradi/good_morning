7cf5405d2e00ddda4090294f1816bec845986c7d
amoradi <moradi.aaron@gmail.com>
2021-04-15 10:46:25 -0400

    add TODO: array type

--- db/setup.sql
+++ db/setup.sql
@@ -3,7 +3,9 @@
   Holds all SQL statements needed to model the app,
   in case it's ever needed for later use.
 
-  varchar 100 & 10 used for consistancy
+  NOTE: varchar 100 & 10 used for consistancy
+
+  TODO: array type is not implemented
 
 */
 
@@ -16,24 +18,27 @@ CREATE TABLE users (
 /* 
 
   Tracks asset **ownership and amount owned** not price.
-  Primary key denotes ownership -- in the form of a symbol-username pair.
+  Primary key denotes ownership -- in the form of a symbol-owner pair.
 
-  - symbol, user_username <PK>
-  - user_username <FK>
+  - symbol, owner <PK>
+  - owner <FK>
 
 */
-CREATE TABLE user_holdings (
+CREATE TABLE holdings (
   symbol: varchar(10) NOT NULL,
   name: varchar(100) NOT NULL,
+  /* TODO: array type */
   category: varchar(100)[] NOT NULL,
   amount: decimal NOT NULL,
-  user_username: varchar(100) NOT NULL REFERENCES users(username),
-  PRIMARY KEY(symbol, user_username)
+  owner: varchar(100) NOT NULL REFERENCES users(username),
+  PRIMARY KEY(symbol, owner)
 )
 
 /*
 
-  Tracks price history for said asset.
+  Tracks price history for said asset. A Low-fi means of obtaining price data, vs 
+  paying for a service. Data is polled from free API(s) and "made ours" by inserting
+  it here.
 
   Asset price history is tracked based on ownership right now (holdings table). If a user
   owns an asset, its price should be tracked. If no users own an asset, it should be removed
@@ -42,6 +47,7 @@ CREATE TABLE user_holdings (
 */
 CREATE TABLE asset_price_history (
   symbol: varchar(10) NOT NULL UNIQUE PRIMARY KEY
+  /* TODO: array type */
   price_at_5pm_usd: [varchar(100), money][] NOT NULL /* [[date, price]] */
   /* other timestamps?? */
 )
